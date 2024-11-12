3a117121141128264ece1ba444203992c90592a4
Gretel <gdelapea@fiu.edu>
2024-11-12 17:09:54 -0500

    new test for l2vpn

--- tests/test_05_l2vpn.py
+++ tests/test_05_l2vpn.py
@@ -95,11 +95,11 @@ class TestE2EL2VPN:
             "name": "Test L2VPN request 2",
             "endpoints": [
                 {
-                    "port_id": "urn:sdx:port:tenet.ac.za:Tenet03:50",
+                    "port_id": "urn:sdx:port:ampath.net:Ampath3:50",
                     "vlan": "any",
                 },
                 {
-                    "port_id": "urn:sdx:port:ampath.net:Ampath3:50",
+                    "port_id": "urn:sdx:port:tenet.ac.za:Tenet03:50",
                     "vlan": "any",
                 },
             ],
@@ -132,7 +132,112 @@ class TestE2EL2VPN:
         response = requests.get("http://tenet:8181/api/kytos/mef_eline/v2/evc/")
         assert len(response.json()) == 2, response.text
 
-    def test_030_delete_l2vpn_successfully(self):
+    def test_040_edit_vlan_l2vpn_successfully(self):
+        """Test change the vlan of endpoints of an existing L2vpn connection."""
+        
+        ## -> ampath
+        response = requests.get("http://ampath:8181/api/kytos/mef_eline/v2/evc/")
+        evcs = response.json()
+        found = 0
+        for evc in evcs.values():
+            if evc.get("uni_a", {}).get("tag", {}).get("value") == 100:
+                found += 1
+        assert found == 0, evcs
+        ## -> tenet
+        response = requests.get("http://tenet:8181/api/kytos/mef_eline/v2/evc/")
+        evcs = response.json()
+        found = 0
+        for evc in evcs.values():
+            if evc.get("uni_z", {}).get("tag", {}).get("value") == 100:
+                found += 1
+        assert found == 0, evcs
+
+        api_url = SDX_CONTROLLER + '/l2vpn/1.0'
+        response = requests.get(api_url)
+        data = response.json()
+
+        # Change vlan
+        key = list(data.keys())[0]
+        current_data = data[key]  
+        payload = {
+            "name": "New vlan in endpoints",
+            "endpoints": [
+                {
+                    "port_id": current_data["endpoints"][0]["port_id"],
+                    "vlan": "100",
+                },
+                {
+                    "port_id": current_data["endpoints"][1]["port_id"],
+                    "vlan": "100",
+                },
+            ],
+        }
+        response = requests.patch(f"{api_url}/{key}", json=payload)
+        assert response.status_code == 201, response.text
+
+        response = requests.get(api_url)
+        data = response.json()
+        current_data = data[key]  
+        assert current_data["name"] == "New vlan in endpoints", str(data)
+        assert current_data["endpoints"][0]["vlan"] == "100", str(data)
+        assert current_data["endpoints"][1]["vlan"] == "100", str(data)
+
+        # give enough time to SDX-Controller to propagate change to OXPs
+        time.sleep(10)
+
+        # make sure OXPs have the new EVCs
+
+        ## -> ampath
+        response = requests.get("http://ampath:8181/api/kytos/mef_eline/v2/evc/")
+        evcs = response.json()
+        found = 0
+        for evc in evcs.values():
+            if evc.get("uni_a", {}).get("tag", {}).get("value") == 100:
+                found += 1
+        assert found == 1, evcs
+        ## -> tenet
+        response = requests.get("http://tenet:8181/api/kytos/mef_eline/v2/evc/")
+        evcs = response.json()
+        found = 0
+        for evc in evcs.values():
+            if evc.get("uni_z", {}).get("tag", {}).get("value") == 100:
+                found += 1
+        assert found == 1, evcs
+
+    def test_045_edit_port_l2vpn_successfully(self):
+        """Test change the port_id of endpoints of an existing L2vpn connection."""
+        api_url = SDX_CONTROLLER + '/l2vpn/1.0'
+        response = requests.get(api_url)
+        data = response.json()
+        key = list(data.keys())[0]
+        current_data = data[key]  
+        assert current_data["endpoints"][0]["port_id"] in ["urn:sdx:port:tenet.ac.za:Tenet03:50", \
+                                                           "urn:sdx:port:ampath.net:Ampath3:50"], str(data)
+        assert current_data["endpoints"][1]["port_id"] in ["urn:sdx:port:tenet.ac.za:Tenet03:50", \
+                                                           "urn:sdx:port:ampath.net:Ampath3:50"], str(data)
+            
+        # Change port_id
+        payload = {
+            "name": "New port_id in endpoints",
+            "endpoints": [
+                {
+                    "port_id": "urn:sdx:port:tenet.ac.za:Tenet01:41",
+                },
+                {
+                    "port_id": "urn:sdx:port:sax.net:Sax01:40",
+                },
+            ],
+        }
+        response = requests.patch(f"{api_url}/{key}", json=payload)
+        assert response.status_code == 201, response.text
+
+        response = requests.get(api_url + f'/{key}/archived')
+        data = response.json()
+        current_data = data[key] 
+        assert current_data["current_path"][0]["port_id"] == "urn:sdx:port:tenet.ac.za:Tenet01:41", str(data)
+        assert current_data["current_path"][-1]["port_id"] == "urn:sdx:port:sax.net:Sax01:40", str(data)
+
+    def test_050_delete_l2vpn_successfully(self):
         """Test deleting all two L2VPNs successfully."""
         api_url = SDX_CONTROLLER + '/l2vpn/1.0'
         response = requests.get(api_url)
