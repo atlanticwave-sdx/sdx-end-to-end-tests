cca4792fdac32717ed7374ea2aa1f17500be294f
Gretel <gdelapea@fiu.edu>
2025-05-13 21:15:10 -0400

    tests for new use cases

--- tests/test_10_use_cases.py
+++ tests/test_10_use_cases.py
@@ -37,6 +37,7 @@ class TestUseCases:
         for l2vpn in response_json:
             response = requests.delete(api_url+f'/{l2vpn}')
         cls.net.config_all_links_up()
+        cls.net.config_all_ports_up()
         time.sleep(15)
 
     def _create_l2vpn(self, port_1, port_2, vlan1="100", vlan2="100"):
@@ -153,7 +154,7 @@ class TestUseCases:
             that port is part of an inter-domain link.
         '''
         key = self._create_l2vpn("urn:sdx:port:ampath.net:Ampath1:50","urn:sdx:port:tenet.ac.za:Tenet01:50")
-
+        
         h1, h6 = self.net.net.get('h1', 'h6')
         h1.cmd('ip link add link %s name vlan100 type vlan id 100' % (h1.intfNames()[0]))
         h1.cmd('ip link set up vlan100')
@@ -177,7 +178,7 @@ class TestUseCases:
         # test connectivity
         assert ', 0% packet loss,' in h1.cmd('ping -c4 10.1.1.6')
 
-        ### Reset 
+        ### Reset - use case 5 (port is just an addition - a new inter-domain path)
         Ampath1.intf('Ampath1-eth40').ifconfig('up') 
 
         time.sleep(15)
@@ -190,7 +191,7 @@ class TestUseCases:
 
     @pytest.mark.xfail(reason="The status of the L2VPN doesn't change to down after setting the link to down and ensure that provisioning is not possible")
     def test_016_update_port_in_inter_domain_link_down_no_reprov(self):
-        ''' Use case 2
+        ''' Use case 2, use case 5
             OXPO sends a topology update with a Port Down and 
             that port is part of an inter-domain link.
         '''
@@ -207,6 +208,7 @@ class TestUseCases:
         # test connectivity
         assert ', 0% packet loss,' in h1.cmd('ping -c4 10.1.1.6')
 
+        # Ampath1-eth40
         self.net.net.configLinkStatus('Ampath1', 'Sax01', 'down')
 
         #  Cause no further (re)provisioning to be possible
@@ -221,7 +223,7 @@ class TestUseCases:
         # test connectivity
         assert ', 100% packet loss,' in h1.cmd('ping -c4 10.1.1.6')
 
-        ### Reset 
+        ### Reset - use case 5 (port UP can benefit the environment (L2VPNs were down because of the lack of paths)
         self.net.net.configLinkStatus('Ampath1', 'Sax01', 'up')
         self.net.net.configLinkStatus('Tenet01', 'Sax01', 'up')
         self.net.net.configLinkStatus('Tenet01', 'Tenet02', 'up')
@@ -238,12 +240,12 @@ class TestUseCases:
 
     @pytest.mark.xfail(reason="The status of the L2VPN doesn't change to down after setting the port to down")
     def test_020_update_uni_port_down(self):
-        ''' Use case 3
+        ''' Use case 3, use case 6
             OXPO sends a topology update with a Port Down and 
             that port is an UNI for some L2VPN.
         '''
         key = self._create_l2vpn("urn:sdx:port:ampath.net:Ampath1:50","urn:sdx:port:tenet.ac.za:Tenet01:50")
-
+        
         h1, h6 = self.net.net.get('h1', 'h6')
         h1.cmd('ip link add link %s name vlan100 type vlan id 100' % (h1.intfNames()[0]))
         h1.cmd('ip link set up vlan100')
@@ -267,7 +269,7 @@ class TestUseCases:
         # test connectivity
         assert ', 100% packet loss,' in h1.cmd('ping -c4 10.1.1.6')
 
-        ### Reset 
+        ### Reset - use case 6 - configs should not be removed in case of a Port Down (data plane config is already there)
         Tenet01.intf('Tenet01-eth50').ifconfig('up') 
 
         time.sleep(15)
@@ -323,8 +325,266 @@ class TestUseCases:
         data = requests.get(api_url).json()
         assert data[key]["status"] == "up"
 
-    def test_030_update_port_in_inter_domain_link_up(self):
+    def test_030_update_port_in_inter_domain_link_up_new_path(self):
         ''' Use case 5
             OXPO sends a topology update with a Port UP and 
             that port is an inter-domain link.
+            If the port is just an addition (a new inter-domain path), do nothing 
+        '''
+        key = self._create_l2vpn("urn:sdx:port:ampath.net:Ampath1:50","urn:sdx:port:tenet.ac.za:Tenet01:50")
+
+        h1, h6 = self.net.net.get('h1', 'h6')
+        h1.cmd('ip link add link %s name vlan100 type vlan id 100' % (h1.intfNames()[0]))
+        h1.cmd('ip link set up vlan100')
+        h1.cmd('ip addr add 10.1.1.1/24 dev vlan100')
+        h6.cmd('ip link add link %s name vlan100 type vlan id 100' % (h6.intfNames()[0]))
+        h6.cmd('ip link set up vlan100')
+        h6.cmd('ip addr add 10.1.1.6/24 dev vlan100')
+
+        # test connectivity
+        assert ', 0% packet loss,' in h1.cmd('ping -c4 10.1.1.6')
+
+        # Ampath1-eth40
+        self.net.net.configLinkStatus('Ampath1', 'Sax01', 'down')
+
+        time.sleep(15)
+
+        api_url = SDX_CONTROLLER + '/l2vpn/1.0'
+        data = requests.get(api_url).json()
+
+        # test connectivity
+        assert ', 0% packet loss,' in h1.cmd('ping -c4 10.1.1.6')
+
+        ### Reset - use case 5 (If the port is just an addition (a new inter-domain path), do nothing )
+        self.net.net.configLinkStatus('Ampath1', 'Sax01', 'up')
+
+        time.sleep(15)
+
+        assert data[key]["status"] == "up" ### FAIL HERE
+
+        data = requests.get(api_url).json()
+        assert data[key]["status"] == "up"
+
+        # test connectivity
+        assert ', 0% packet loss,' in h1.cmd('ping -c4 10.1.1.6')
+
+    @pytest.mark.xfail(reason="The status of the L2VPN doesn't change to down after setting the link to down")
+    def test_40_update_port_uni_up_no_l2vpn_associated(self):
+        ''' Use case 7
+            OXPO sends a topology update with a Port UP and 
+            that port has no L2VPN associated with it. 
+            
+            Expected behavior: For a UNI port, the SDX-Controller does nothing.
+        '''
+        key = self._create_l2vpn("urn:sdx:port:ampath.net:Ampath1:50","urn:sdx:port:tenet.ac.za:Tenet01:50")
+        port = 'urn:sdx:port:ampath.net:Ampath2:50'
+
+        api_url_topology = SDX_CONTROLLER + '/topology'
+        response = requests.get(api_url_topology)
+        data = response.json()
+        ports = {port["id"] for node in data["nodes"] for port in node["ports"] if port['nni'] == ''}
+        # UNI port
+        assert port in ports
+
+        self.net.net.configLinkStatus('Tenet01', 'Tenet03', 'down') 
+        time.sleep(15)
+
+        api_url = SDX_CONTROLLER + '/l2vpn/1.0'
+        initial_data = requests.get(api_url).json()
+        path_ports = [p['port_id'] for p in initial_data[key]['current_path']]
+        # Port has no L2VPN associated
+        assert port not in path_ports
+        assert initial_data[key]["status"] == "down"   ### FAIL HERE
+
+        # Simulate port UP to trigger topology update
+        self.net.net.configLinkStatus('Tenet01', 'Tenet03', 'up')
+        time.sleep(15)
+
+        # Verify no L2VPN was created or modified
+        final_data = requests.get(api_url).json()
+        assert final_data == initial_data, "L2VPN state changed unexpectedly"
+
+    @pytest.mark.xfail(reason="Link is not removed from topology after being deleted")
+    def test_50_update_link_missing(self):
+        ''' Use case 8
+            OXPO sends a topology update with a Link missing (deleted by the OXP)
+
+            Expected behavior: 
+            Topology version number increases, 
+            link is removed from topology,
+            L2VPN status changes to down due to no alternate path exists,
+            the Link is not exported by the OXP and SDX-LC,
+        '''
+        # Create L2VPN
+        key = self._create_l2vpn("urn:sdx:port:tenet.ac.za:Tenet01:50", "urn:sdx:port:tenet.ac.za:Tenet03:50")
+        link = 'urn:sdx:link:tenet.ac.za:Tenet01/2_Tenet03/2' 
+
+        # Configure hosts
+        h6, h8 = self.net.net.get('h6', 'h8')
+        h6.cmd('ip link add link %s name vlan100 type vlan id 100' % (h6.intfNames()[0]))
+        h6.cmd('ip link set up vlan100')
+        h6.cmd('ip addr add 10.1.1.6/24 dev vlan100')
+        h8.cmd('ip link add link %s name vlan100 type vlan id 100' % (h8.intfNames()[0]))
+        h8.cmd('ip link set up vlan100')
+        h8.cmd('ip addr add 10.1.1.8/24 dev vlan100')
+
+        # Test initial connectivity
+        assert ', 0% packet loss,' in h6.cmd('ping -c4 10.1.1.8')
+
+        # Get initial topology version
+        api_url_topology = SDX_CONTROLLER + '/topology'
+        initial_topology = requests.get(api_url_topology).json()
+        initial_version = float(initial_topology["version"])
+        links = {link["id"] for link in initial_topology["links"]}
+        assert link in links
+
+        # Simulate link deletion by setting link to down
+        self.net.net.configLinkStatus('Tenet01', 'Tenet03', 'down')
+        time.sleep(15) 
+
+        # Verify topology version increased
+        updated_topology = requests.get(api_url_topology).json()
+        updated_version = float(updated_topology["version"])
+        assert updated_version > initial_version, "Topology version did not increase"
+
+        links = {link["id"]: link for link in updated_topology["links"]}
+        assert link not in links   ### FAIL HERE
+
+        # Verify L2VPN status is down (no alternate path)
+        api_url = SDX_CONTROLLER + '/l2vpn/1.0'
+        data = requests.get(api_url).json()
+        assert data[key]["status"] == "down", "L2VPN status did not change to down"
+
+        # Test connectivity (should fail)
+        assert ', 100% packet loss,' in h6.cmd('ping -c4 10.1.1.8')
+
+        # Verify Link is not exported by tenet and SDX-LC
+        tenet_api = KYTOS_API % 'tenet'
+        response = requests.get(f'{tenet_api}/topology/v3/links')
+        assert response.status_code == 200
+        data = response.json()
+        for _, link_ in data['links'].items():
+            ep_a = link_['endpoint_a']['name'].split('-')[0]
+            ep_b = link_['endpoint_b']['name'].split('-')[0]
+            assert set(['Tenet01', 'Tenet03']) != set([ep_a, ep_b]), link_
+
+        sdx_api = KYTOS_SDX_API % 'tenet'
+        response = requests.get(f"{sdx_api}/topology/2.0.0")
+        assert response.status_code == 200
+        data = response.json()
+        links = [l['id'] for l in data['links']]
+        assert link not in links
+
+        # Reset: Restore link
+        self.net.net.configLinkStatus('Tenet01', 'Tenet03', 'up')
+        time.sleep(15)
+
+        # Verify L2VPN status is up
+        data = requests.get(api_url).json()
+        assert data[key]["status"] == "up"
+
+        # Test connectivity (should succeed)
+        assert ', 0% packet loss,' in h6.cmd('ping -c4 10.1.1.8')
+        
+    @pytest.mark.xfail(reason="Link is not removed from topology after being deleted")
+    def test_55_update_link_missing_with_alternate_path(self):
+        ''' Use case 8
+            OXPO sends a topology update with a Link missing (deleted by the OXP)
+
+            Expected behavior: 
+            Topology version number increases, 
+            link is removed from topology,
+            the Link is not exported by the OXP and SDX-LC,
+        '''
+        # Create L2VPN
+        key = self._create_l2vpn("urn:sdx:port:tenet.ac.za:Tenet01:50", "urn:sdx:port:tenet.ac.za:Tenet02:50")
+        link = 'urn:sdx:link:tenet.ac.za:Tenet01/1_Tenet02/1' 
+
+        # Configure hosts
+        h6, h7 = self.net.net.get('h6', 'h7')
+        h6.cmd('ip link add link %s name vlan100 type vlan id 100' % (h6.intfNames()[0]))
+        h6.cmd('ip link set up vlan100')
+        h6.cmd('ip addr add 10.1.1.6/24 dev vlan100')
+        h7.cmd('ip link add link %s name vlan100 type vlan id 100' % (h7.intfNames()[0]))
+        h7.cmd('ip link set up vlan100')
+        h7.cmd('ip addr add 10.1.1.7/24 dev vlan100')
+
+        # Test initial connectivity
+        assert ', 0% packet loss,' in h6.cmd('ping -c4 10.1.1.7')
+
+        # Get initial topology version
+        api_url_topology = SDX_CONTROLLER + '/topology'
+        initial_topology = requests.get(api_url_topology).json()
+        initial_version = float(initial_topology["version"])
+        links = {link["id"] for link in initial_topology["links"]}
+        assert link in links
+
+        # Simulate link deletion by setting link to down
+        self.net.net.configLinkStatus('Tenet01', 'Tenet02', 'down')
+        time.sleep(15) 
+
+        # Verify topology version increased
+        updated_topology = requests.get(api_url_topology).json()
+        updated_version = float(updated_topology["version"])
+        assert updated_version > initial_version, "Topology version did not increase"
+
+        links = {link["id"]: link for link in updated_topology["links"]}
+        assert link not in links   ### FAIL HERE
+
+        # Verify L2VPN status is up (alternate path)
+        api_url = SDX_CONTROLLER + '/l2vpn/1.0'
+        data = requests.get(api_url).json()
+        assert data[key]["status"] == "up"
+
+        # Test connectivity (should fail)
+        assert ', 0% packet loss,' in h6.cmd('ping -c4 10.1.1.8')
+
+        # Verify Link is not exported by tenet and SDX-LC
+        tenet_api = KYTOS_API % 'tenet'
+        response = requests.get(f'{tenet_api}/topology/v3/links')
+        assert response.status_code == 200
+        data = response.json()
+        for _, link_ in data['links'].items():
+            ep_a = link_['endpoint_a']['name'].split('-')[0]
+            ep_b = link_['endpoint_b']['name'].split('-')[0]
+            assert set(['Tenet01', 'Tenet02']) != set([ep_a, ep_b]), link_
+
+        sdx_api = KYTOS_SDX_API % 'tenet'
+        response = requests.get(f"{sdx_api}/topology/2.0.0")
+        assert response.status_code == 200
+        data = response.json()
+        links = [l['id'] for l in data['links']]
+        assert link not in links
+
+        # Reset: Restore link
+        self.net.net.configLinkStatus('Tenet01', 'Tenet02', 'up')
+        time.sleep(15)
+
+        # Verify L2VPN status is up
+        data = requests.get(api_url).json()
+        assert data[key]["status"] == "up"
+
+        # Test connectivity (should succeed)
+        assert ', 0% packet loss,' in h6.cmd('ping -c4 10.1.1.8')
+        
+    def test_70_update_port_nni_missing(self):
+        ''' Use case 9
+            OXPO sends a topology update with a Port missing
+        '''
+
+    def test_75_update_port_uni_missing(self):
+        ''' Use case 9
+            OXPO sends a topology update with a Port missing
+        '''
+
+    def test_80_update_changed_vlan_range_on_nni(self):
+        ''' Use case 10
+            OXPO sends a topology update with a changed VLAN range 
+            for any of the services supported.
+        '''
+
+    def test_85_update_changed_vlan_range_on_uni(self):
+        ''' Use case 10
+            OXPO sends a topology update with a changed VLAN range 
+            for any of the services supported.
         '''
