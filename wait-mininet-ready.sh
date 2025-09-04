841bc6ef5abc83cb327cda7f530a203b7a2f4c30
Italo Valcy <italovalcy@gmail.com>
2025-09-04 05:45:07 -0400

    adding some info on wait-mininet-ready

--- wait-mininet-ready.sh
+++ wait-mininet-ready.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Waiting mininet to be ready"
+echo -n "Waiting mininet to be ready..."
 while ! docker compose logs mininet -n 1 | grep -q "tail -f /dev/null"; do
 	sleep 2
 	echo -n "."
